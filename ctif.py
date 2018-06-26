edf076962a2af7854a45d1019fb23c4029365751
kalpana <kalpanathangavel95@gmail.com>
2018-06-26 17:57:16 +0530

    all files added

---
+++ ctif.py
@@ -0,0 +1,5 @@
+def ctof(c) :
+	f=9/5*c+32
+	print(f)
+
+ctof(100)
---
+++ newline.py
@@ -0,0 +1,17 @@
+def newline():
+	print()
+	
+print("firstline")
+newline()
+print("second line")
+
+
+	
+def threeline():
+	print()
+print("firstline")
+threeline()
+print("second line")
+threeline()
+print("third line")
+
